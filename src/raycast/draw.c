f5a8f94d64bdf42a39df2f64eb2c0afb0e7665c7
Agnes Tshibangu <agnes.tshibangugrillon@gmail.com>
2024-12-19 16:48:58 +0100

    textures NSEW

--- src/raycast/draw.c
+++ src/raycast/draw.c
@@ -8,8 +8,6 @@ void verLine(int x, int drawStart, int drawEnd, t_data *data, int textureIndex, 
 
     int textureWidth = data->texture[textureIndex].width;
     int textureHeight = data->texture[textureIndex].height;
-    printf(" textureWidth %d\n",  textureWidth);
-	 printf(" textureHeight %d\n",  textureHeight);
     double step = (double)textureHeight / (drawEnd - drawStart);
     double texturePos = 0.0;
     // int textureX = (x % textureWidth);
